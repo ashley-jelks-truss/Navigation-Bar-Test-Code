6c68da3438f7eae13beb5e22ee69a0c223aa4b7c
Ashley Jelks <ashley@truss.works>
2015-12-31 11:25:49 -0800

    first commit of nav bar test code

---
+++ Navigation Bar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Bar
+//
+//  Created by Ashley Jelks on 12/7/15.
+//  Copyright © 2015 Ashley Jelks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
